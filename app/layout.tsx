04b349cc7c11af37be8693bc6db28ab16bb5a1a7
Ваня <Vanya@MacBook-Air-Liza.local>
2026-05-19 14:01:35 +0300

    catalog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}
+        <Script src="https://telegram.org/js/telegram-web-app.js" />
+      </body>
+
     </html>
   );
 }
